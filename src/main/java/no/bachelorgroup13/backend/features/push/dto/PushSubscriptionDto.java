007bd0bd1051eebec7fcda55695788f1fc857977
Viljar Hoem-Olsen <viljar.hoem.olsen@gmail.com>
2025-05-08 14:35:33 +0200

    BAC-99: Minor cleanup in push dto

--- src/main/java/no/bachelorgroup13/backend/features/push/dto/PushSubscriptionDto.java
+++ src/main/java/no/bachelorgroup13/backend/features/push/dto/PushSubscriptionDto.java
@@ -21,31 +21,6 @@ public class PushSubscriptionDto {
 
     private UUID userId;
 
-    // Add getters and setters
-    public String getEndpoint() {
-        return endpoint;
-    }
-
-    public void setEndpoint(String endpoint) {
-        this.endpoint = endpoint;
-    }
-
-    public String getP256dh() {
-        return p256dh;
-    }
-
-    public void setP256dh(String p256dh) {
-        this.p256dh = p256dh;
-    }
-
-    public String getAuth() {
-        return auth;
-    }
-
-    public void setAuth(String auth) {
-        this.auth = auth;
-    }
-
     @Override
     public String toString() {
         return "PushSubscriptionDto{"
